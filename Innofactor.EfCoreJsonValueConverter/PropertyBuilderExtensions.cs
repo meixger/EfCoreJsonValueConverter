4a24556bf786b631b135dd28773ae93e5e391ecb
Martin Meixger <martin@meixger.com>
2020-09-17 08:52:57 +0200

    WIP feat AddJsonField
    Don't require [JsonField] on model for leaner blazor wasm models

--- Innofactor.EfCoreJsonValueConverter/PropertyBuilderExtensions.cs
+++ Innofactor.EfCoreJsonValueConverter/PropertyBuilderExtensions.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Innofactor.EfCoreJsonValueConverter {
 
@@ -16,6 +19,29 @@ namespace Innofactor.EfCoreJsonValueConverter {
       propertyBuilder
         .HasConversion(new JsonValueConverter<T>())
         .Metadata.SetValueComparer(new JsonValueComparer<T>());
+
+      return propertyBuilder;
+
+    }
+
+    /// <summary>
+    /// Serializes field as JSON blob in database.
+    /// </summary>
+    public static PropertyBuilder<T> AddJsonField<T>(this PropertyBuilder<T> propertyBuilder) where T : class {
+
+      propertyBuilder
+        .HasConversion(new JsonValueConverter<T>())
+        .Metadata.SetValueComparer(new JsonValueComparer<T>());
+
+      var modelType = typeof(T);
+      var property = propertyBuilder;
+
+      var converterType = typeof(JsonValueConverter<>).MakeGenericType(modelType);
+      var converter = (ValueConverter)Activator.CreateInstance(converterType, new object[] { null });
+      property.Metadata.SetValueConverter(converter);
+
+      var valueComparer = typeof(JsonValueComparer<>).MakeGenericType(modelType);
+      property.Metadata.SetValueComparer((ValueComparer)Activator.CreateInstance(valueComparer, new object[0]));
 
       return propertyBuilder;
 
